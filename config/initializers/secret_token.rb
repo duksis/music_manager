8f783ba22b3f3ce460bd87edb5a0793fd6b9fd6f
Hugo Duksis <hugo.duksis@tieto.com>
2012-11-22 23:55:55 +0200

    Rails ready, set, go!
    Clean rails 3.2.9 app with rspce, steak, guard, 
    factory_girl and database_cleaner

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicManager::Application.config.secret_token = 'b5e36e5d816fbf32ba06862f8ac2e191ca969bdd0f6c458ac939d487c8a88cce7804764427a7062ae7f43148adbead8e73db973c9718d68577f28a1b89277da3'
